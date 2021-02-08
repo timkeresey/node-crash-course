f4b9a1135a09e9916ff6a03c7f4aa138eb0bc61e
Tim Keresey <tim.keresey@gmail.com>
2021-02-08 16:46:21 -0700

    extend event emitter class

---
+++ reference/event-demo.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+// Create emitter class
+class MyEmitter extends EventEmitter { }
+
+// Init object
+const myEmitter = new MyEmitter();
+
+// Event listener
+myEmitter.on('event', () => console.log('event fired!'));
+
+// Init event
+myEmitter.emit('event');
+myEmitter.emit('event');
+
+//
