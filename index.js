58a5e93d3a5cd97181626d10cb3c6059315eb286
Tim Keresey <tim.keresey@gmail.com>
2021-02-09 14:24:49 -0700

    send file response

--- index.js
+++ index.js
@@ -52,6 +52,27 @@ const server = http.createServer((req, res) => {
       contentType = 'image/jpg';
       break;
   }
+
+  // Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // Page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content, 'utf8');
+        })
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, 'utf8');
+    }
+  })
 });
 
 // when deployed, host will decide which port to run on in an environment variable(pocess.env.PORT), so we want to listen for that variable, or run on 5000.
