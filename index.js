87eb22cdee501e77e1813d4e821bc30e813a8646
Tim Keresey <tim.keresey@gmail.com>
2021-02-08 17:29:47 -0700

    create basic server

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const Person = require('./person');
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // Content type
+    res. writeHead(200, { "Content-Type": "text/html"})
+
+    res.end('<h1>Homepage</h1>')
+  }
+});
+
+// when deployed, host will decide which port to run on in an environment variable(pocess.env.PORT), so we want to listen for that variable, or run on 5000.
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+//create an npm script for nodemon (live updating) because we did not install nodemon globally. (npm run dev)
