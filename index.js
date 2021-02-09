ddeeb3c6beb551fda30fdea769f449de65ef9196
Tim Keresey <tim.keresey@gmail.com>
2021-02-09 14:10:52 -0700

    set file ath and content type

--- index.js
+++ index.js
@@ -3,22 +3,54 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-      if (err) throw err;
-      // Content type
-      res.writeHead(200, { "Content-Type": "text/html"});
-      res.end(content);
-    });
-  }
+  // if (req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //     if (err) throw err;
+  //     // Content type
+  //     res.writeHead(200, { "Content-Type": "text/html"});
+  //     res.end(content);
+  //   });
+  // }
+  //
+  // if (req.url === '/api/users') {
+  //   const users = [
+  //     { name: 'Bob Smith', age: 40 },
+  //     { name: 'Sally Smith', age: 35 }
+  //   ];
+  //   res.writeHead(200, { "Content-Type": "application/json"});
+  //   res.end(JSON.stringify(users));
+  // }
 
-  if (req.url === '/api/users') {
-    const users = [
-      { name: 'Bob Smith', age: 40 },
-      { name: 'Sally Smith', age: 35 }
-    ];
-    res.writeHead(200, { "Content-Type": "application/json"});
-    res.end(JSON.stringify(users));
+  // Build file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content Type
+  let contentType = 'text/html';
+
+  // Check ext and set content type
+  switch(extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+
+    case '.css':
+      contentType = 'text/css';
+      break;
+
+    case '.json':
+      contentType = 'application/json';
+      break;
+
+    case '.png':
+      contentType = 'image/png';
+      break;
+
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
 });
 
